b14aa74e8c553c634052b46bb07e9b668ddd64bc
newtonkiragu <karanunewton4@gmail.com>
2017-06-19 13:20:37 +0300

    commented out the nav-bar but will return it later

--- app/components/brand-name.js
+++ app/components/brand-name.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    model: function() {
+      var key = config.myApiKey;
+      console.log(config.myApiKey);
+      var url = 'http://api.shopstyle.com/api/v2/brands?pid=uid8096-39509436-69';
+      return Ember.$.getJSON(url).then(function(responseJSON) {
+        //use console log this to identify the objects
+        console.log(responseJSON.products);
+        return responseJSON.product;
+      });
+    },
+  }
 });
