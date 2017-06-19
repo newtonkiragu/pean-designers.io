73bb7747fbf59206bceea247c42067510952a05b
pmutua <pmutua@live.com>
2017-06-19 16:09:37 +0300

    created custom_stripe.js file and added logic for responding with checkout-payment.hbs template credit card form

---
+++ vendor/custom_stripe.js
@@ -0,0 +1,17 @@
+/ Event Listeners
+$('#payment-form').on('submit', generateToken);
+ 
+var generateToken = function(e) {
+    var form = $(this);
+ 
+    // No pressing the buy now button more than once
+    form.find('button').prop('disabled', true);
+ 
+    // Create the token, based on the form object
+    Stripe.create(form, stripeResponseHandler);
+ 
+    // Prevent the form from submitting
+    e.preventDefault();
+};
+ 
+var stripeResponseHandler = function(status, response) {};
