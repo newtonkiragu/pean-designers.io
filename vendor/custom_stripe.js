26ba7590bed40331ac0cceaeef7850b61927baba
pmutua <pmutua@live.com>
2017-06-19 16:23:17 +0300

    Within this callback,its important to verify the result(*was all of the information provided correctly*),insert a token into a hidden *input* and submit the form to our server-only the token and non-sensitive data. This is important therefore i have written the acceptance/functional tests to verify it

--- vendor/custom_stripe.js
+++ vendor/custom_stripe.js
@@ -14,4 +14,34 @@ var generateToken = function(e) {
     e.preventDefault();
 };
  
-var stripeResponseHandler = function(status, response) {};
+
+//Within this callback, it’s important to verify the result (was all of 
+//the information provided correctly), insert the token into a hidden input,
+//and submit the form to your server. Again, note that the credit card information should/will not hit your server - only the token and
+//non-sensitive data. This is important, so write acceptance or functional tests to verify it.
+
+var stripeResponseHandler = function(status, response) {
+    var form = $('#payment-form');
+ 
+    // Any validation errors?
+    if (response.error) {
+        // Show the user what they did wrong
+        form.find('.payment-errors').text(response.error.message);
+ 
+        // Make the submit clickable again
+        form.find('button').prop('disabled', false);
+    } else {
+        // Otherwise, we're good to go! Submit the form.
+ 
+        // Insert the unique token into the form
+        $('<input>', {
+            'type': 'hidden',
+            'name': 'stripeToken',
+            'value': response.id
+        }).appendTo(form);
+ 
+        // Call the native submit method on the form
+        // to keep the submission from being canceled
+        form.get(0).submit();
+    }
+};
