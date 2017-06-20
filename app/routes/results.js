9e08310d048ac91c0de73f3e217fb75933543559
newtonkiragu <karanunewton4@gmail.com>
2017-06-20 15:51:42 +0300

    made the code dry by removing all the commented code

--- app/routes/results.js
+++ app/routes/results.js
@@ -4,19 +4,17 @@ import config from '../config/environment';
 export default Ember.Route.extend({
 
 
-model: function(params) {
-   var key = config.myApiKey;
-   console.log(config.myApiKey);
-   var url = 'http://api.shopstyle.com/api/v2/products?pid=uid4089-39490717-44&fts='+params.name+'&offset=0&limit=10' ;
-   return Ember.$.getJSON(url).then(function(responseJSON) {
-    //use console log this to identify the objects 
-     // console.log(responseJSON.products);
-     return responseJSON.products;
-   });
+  model: function(params) {
+    var key = config.myApiKey;
+    console.log(config.myApiKey);
+    var url = 'http://api.shopstyle.com/api/v2/products?pid=uid4089-39490717-44&fts=' + params.name + '&offset=0&limit=10';
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.products;
+    });
   },
   actions: {
     addOrder2(params) {
-      var addOrder = this.store.createRecord('order',params);
+      var addOrder = this.store.createRecord('order', params);
       addOrder.save();
     }
   }
